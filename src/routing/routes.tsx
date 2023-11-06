061117f95edf330d77246dd6373086d4fab7edf8
DijanaVN <veljanovskadijana@yahoo.com>
2023-11-06 12:35:05 +0100

    create ErrorPage.tsx

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./../pages/HomePage";
 import Layout from "../components/Layout";
 import AboutPage from "./../pages/AboutPage";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    //   errorElement: <ErrorPage />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "/about", element: <AboutPage /> },
